ce8cedf8ae57c0ba9ea5334d7cd35a581ef0d965
pawel <pawelgagorowski026@gmail.com>
2019-01-12 12:13:37 +0100

    firts commit

---
+++ server/server.js
@@ -0,0 +1,12 @@
+var express = require('express')
+const path = require('path');
+
+const publicPath = path.join(__dirname, "../public");
+const port = process.env || 3000
+const app = express();
+
+app.use(express.static(publicPath))
+
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`)
+})
