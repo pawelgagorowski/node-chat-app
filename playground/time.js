5bd76b9eafba1e5f95e71e19b4581183ac232883
pawel <pawelgagorowski026@gmail.com>
2019-01-13 18:12:33 +0100

    Add join page that submits to chat.html

--- playground/time.js
+++ playground/time.js
@@ -1,9 +1,13 @@
 const moment = require('moment');
 
-var createdAt = 12345;
-const date = moment(createdAt);
-console.log(date.format('MMM Do, YYYY'))
-console.log(date.format('h:mm a'))
+// var createdAt = 12345;
+// const date = moment(createdAt);
+// console.log(date.format('MMM Do, YYYY'))
+// console.log(date.format('h:mm a'))
+//
+// var someTimestamp = moment().valueOf();
+// console.log(someTimestamp)
 
-var someTimestamp = moment().valueOf();
-console.log(someTimestamp)
+
+
+console.log(new Date (Number('1547396716622')))
--- public/js/chat.js
+++ public/js/chat.js
@@ -1,5 +1,22 @@
 var socket = io();
 
+function scrollToBottom() {
+  // Selectors
+  var messages = $('#messages')
+  var newMessage = messages.children('li:last-child')
+
+  //Heights
+  var clientHeight = messages.prop('clientHeight');
+  var scrollTop = messages.prop('scrollTop');
+  var scrollHeight = messages.prop('scrollHeight');
+  var newMessageHeight = newMessage.innerHeight();
+  var lastMessageHeight = newMessage.prev().innerHeight();
+
+  if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+    messages.scrollTop(scrollHeight)
+  }
+}
+
 socket.on('connect', function () {
   console.log("Connected to server");
 })
@@ -9,14 +26,17 @@ socket.on('disconnect', function() {
 })
 
 socket.on('newMessage', function (message) {
-  var formattedTime = moment(message.createdAt).format('h:mm a')
+  // var formattedTime = moment(message.createdAt).format('h:mm a')
+
   var template = $('#message-template').html()
   var html = Mustache.render(template, {
     text: message.text,
     from: message.from,
-    createdAt: formattedTime
+    createdAt: message.createdAt,
+    timeStamp: moment(message.createdAt).fromNow()
   })
   $('#messages').append(html);
+  scrollToBottom();
 
   // var li = $('<li></li>');
   // li.text(`${message.from} ${formattedTime}: ${message.text}`);
@@ -25,7 +45,7 @@ socket.on('newMessage', function (message) {
 })
 
 socket.on('newLocationMessage', function (message) {
-  var formattedTime = moment(message.createdAt).format('h:mm a')
+  var formattedTime = moment(message.createdAt).format('+-h:mm a')
   var template = $('#location-message-template').html()
   var html = Mustache.render(template, {
     from: message.from,
@@ -34,6 +54,7 @@ socket.on('newLocationMessage', function (message) {
   })
 
    $('#messages').append(html)
+   scrollToBottom();
   // var li = $('<li></li>');
   // var a = $('<a target="_blank">My curent location</a>');
   //
@@ -85,3 +106,19 @@ locationButton.on('click', function () {
     alert('Unable to fetch location')
   })
 })
+
+function updateTimeStamps(){
+  let loop = document.getElementById("messages").getElementsByClassName("message").length;
+  for(let i=0; i<loop; i++){
+    let timeStamp = document.getElementById("messages").getElementsByClassName("message")[i].getAttribute("data-timestamp")
+    // console.log(typeof timeStamp)
+    // console.log(timeStamp)
+    // console.log(new Date(Number(timeStamp)))
+    // console.log(moment(new Date(Number(timeStamp))).isValid());
+    document.getElementById("messages").getElementsByClassName("message")[i].getElementsByClassName("timestamp")[0].textContent = moment(new Date(Number(timeStamp))).fromNow();
+    // console.log(moment(new Date(Number(timeStamp))).fromNow())
+  }
+  console.log('TimeStamps updated');
+};
+
+setInterval(updateTimeStamps, 60000);
