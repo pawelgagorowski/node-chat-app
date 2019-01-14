886fafdae6a39ffa4e22f7967e0e8497c01ba245
pawel <pawelgagorowski026@gmail.com>
2019-01-14 22:42:46 +0100

    Add Users class and updateUserList

--- public/js/chat.js
+++ public/js/chat.js
@@ -18,11 +18,30 @@ function scrollToBottom() {
 }
 
 socket.on('connect', function () {
-  console.log("Connected to server");
+  var params = $.deparam(window.location.search);
+
+  socket.emit('join', params, function (err) {
+    if(err) {
+      alert(err)
+      window.location.href = '/';
+    } else {
+      console.log('no error')
+    }
+  })
 })
 
 socket.on('disconnect', function() {
   console.log("Disconnected from server")
+})
+
+socket.on('updateUserList', function (users) {
+  var ol = $('<ol></ol>');
+
+  users.forEach(function(user) {
+    ol.append($('<li></li>').text(user))
+  })
+
+  $('#users').html(ol)
 })
 
 socket.on('newMessage', function (message) {
@@ -66,12 +85,12 @@ socket.on('newLocationMessage', function (message) {
 
 })
 
-socket.emit('createMessage', {
-  from: "Frank",
-  text: "Hi"
-}, function (data) {
-  console.log("Got it", data);
-})
+// socket.emit('createMessage', {
+//   from: "Frank",
+//   text: "Hi"
+// }, function (data) {
+//   console.log("Got it", data);
+// })
 
 $('#message-form').on('submit', function (e) {
   e.preventDefault()
